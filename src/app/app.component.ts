4acc547d8ffdd99a9bed732cc43bd1fefbe2adbf
dyevdushchenko <a2ka7.dp@gmail.com>
2025-03-25 13:11:16 +0200

    clean unnecessary links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'pdp';
+  version = version;
 }
